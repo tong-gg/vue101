3f85b6336136d86172c8c6824c0227cd6fc393b9
Patipan <patipan.bo@ku.th>
2021-07-27 08:07:41 +0700

    pokemon list

---
+++ src/views/PokemonList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <pokemon-create-form></pokemon-create-form>
+        <pokemon-table></pokemon-table>
+    </div>
+</template>
+
+<script>
+import PokemonTable from '@/components/PokemonTable'
+import PokemonCreateForm from '@/components/PokemonCreateForm'
+
+export default {
+    components: {
+        PokemonTable,
+        PokemonCreateForm
+    },
+}
+</script>
+
+<style>
+
+</style>
