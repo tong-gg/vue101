b8645f2e88ac3cde44d7f95ba267e89678328c37
Patipan <patipan.bo@ku.th>
2021-07-27 08:07:56 +0700

    pokemon components

---
+++ src/components/PokemonCreateForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+      Add New Pokemon
+      <div>
+        <div>
+            <label for="name">Name (En)</label>
+            <input type="text" v-model="form.name.english">
+        </div>
+
+        <div>
+            <label for="name_jp">Name (Jp)</label>
+            <input type="text" v-model="form.name.japanese">
+        </div>
+
+        <div>
+            <label for="type">Type</label>
+            <input type="text" v-model="form.type">
+        </div>
+
+        <div>
+            <button @click="addPokemon">Add</button>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import PokemonStore from '@/store/Pokemon'
+export default {
+    data() {
+        return {
+            form: {
+                name: {
+                    english: '',
+                    japanese: ''
+                },
+                type: ''
+            }
+        }
+    },
+    methods: {
+        clearForm() {
+            this.form = {
+            form: {
+                name: {
+                    english: '',
+                    japanese: ''
+                },
+                type: ''
+            }
+        }
+        },
+        addPokemon() {
+            console.log(this.form)
+            let payload = {
+                name: this.form.name,
+                type: this.form.type.split(",").map((item) => item.trim())
+            }
+            // todo: calling action in PokemonStore to insert data
+            console.log(payload)
+            PokemonStore.dispatch('addPokemon', payload)
+
+            this.clearForm()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
